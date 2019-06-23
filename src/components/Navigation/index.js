3aadf3bc71774aca7b0fe9a1e42ce3bd71b194ad
Josh Hoeg <josh.hoeg@du.edu>
2019-06-22 18:31:29 -0600

    styled the navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,6 +5,25 @@ import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const Navigation = () => (
   <div>
     <AuthUserContext.Consumer>
@@ -15,34 +34,44 @@ const Navigation = () => (
   </div>
 );
 
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
-);
+ const NavigationAuth = () => {
+  const classes = useStyles();
 
-const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
-);
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Evolve
+          </Typography>
+          <Button color="inherit"><Link to={ROUTES.LANDING}>Landing</Link></Button>
+          <Button color="inherit"><Link to={ROUTES.HOME}>Home</Link></Button>
+          <Button color="inherit"><Link to={ROUTES.ACCOUNT}>Account</Link></Button>
+          <Button color="inherit"><Link to={ROUTES.ADMIN}>Admin</Link></Button>
+          <Button color="inherit"><SignOutButton /></Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+const NavigationNonAuth = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Evolve
+          </Typography>
+          <Button color="inherit"><Link to={ROUTES.LANDING}>Landing</Link></Button>
+          <Button color="inherit"><Link to={ROUTES.SIGN_IN}>Sign In</Link></Button>
+          <Button color="inherit"><Link to={ROUTES.SIGN_UP}>Sign Up</Link></Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <App />
-  </FirebaseContext.Provider>,
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
 
